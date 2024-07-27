d5a8cc53df39cbeb96e05e5d6f3c21e4b206ec72
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-07-27 20:34:12 +0100

    init

---
+++ test_pet.py
@@ -0,0 +1,94 @@
+import pytest
+import requests
+
+# python -m pytest -v -s .\test_pet.py
+
+endpoint = "https://petstore.swagger.io"
+api = endpoint + "/v2"
+
+
+def test_get_endpoint():
+    get_endpoint_response = requests.get(endpoint)
+    assert get_endpoint_response.status_code == 200
+
+
+def test_post_pet():
+    # create pet
+    pet = new_pet()
+    post_pet_response = post_pet(pet)
+    assert post_pet_response.status_code == 200
+
+    post_pet_data = post_pet_response.json()
+
+    # check the created pet
+    pet_id = post_pet_data["id"]
+    get_pet_response = get_pet(pet_id)
+    assert get_pet_response.status_code == 200
+
+    # check data are same as the ones pushed + correct fail resp
+    get_pet_data = get_pet_response.json()
+    assert get_pet_data["status"] == pet["status"]
+    assert get_pet_data["name"] == pet["name"]
+    # assert get_pet_data["name"] == ["douggie"]  # should fail, correct name: "doggie"
+
+
+def test_update_pet():
+    # create pet
+    pet = new_pet()
+    post_pet_response = post_pet(pet)
+    assert post_pet_response.status_code == 200
+
+    pet_id = post_pet_response.json()["id"]
+
+    # update pet
+    updated_pet = {
+        "id": pet_id,  # generated server side
+        "category": {"id": 0, "name": pet["category"]["name"]},
+        "name": "updated name",
+        "photoUrls": [pet["photoUrls"]],
+        "tags": [{"id": 0, "name": "my tag name"}],
+        "status": "unavailable",
+    }
+    update_pet_response = update_pet(updated_pet)
+    update_pet_data = update_pet_response.json()
+    print("UPDATE_PET DATA:", update_pet_data)
+    # assert update_pet_response.status_code == 200
+    # currently gives a 500
+    # check new pet data to see what get stuck
+
+    # get updated pet, check new data
+
+
+def post_pet(pet):
+    return requests.post(api + "/pet", json=pet)
+
+
+def get_pet(pet_id):
+    return requests.get(api + f"/pet/{pet_id}")
+
+
+def update_pet(pet):
+    return requests.put(api + "/pet", json=pet)
+
+
+def post_image(petId):
+    return requests.post(endpoint + f"/pet/{petId}/uploadImage")
+
+
+def new_pet():
+    return {
+        # "id": 0, # defined server side
+        "category": {
+            # "id": 0, # set to 0 by default server side
+            "name": "test category name"
+        },
+        "name": "doggie",
+        "photoUrls": ["my_photo_url"],
+        "tags": [
+            {
+                # "id": 0, # set to 0 by default server side
+                "name": "my tag name"
+            }
+        ],
+        "status": "available",
+    }
