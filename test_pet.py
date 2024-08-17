cedcc6869158e69340f03470747f101bb12e6ba4
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-08-17 15:19:38 +0100

    test_post_data_form errors

--- test_pet.py
+++ test_pet.py
@@ -452,6 +452,37 @@ def test_post_data_form_unused_id():
     assert post_data_form_response.status_code == 404, f"Failed, gives {post_data_form_response.status_code} instead of 404"
 
 
+# python -m pytest -v -s .\test_pet.py::test_post_data_form_empty_name
+def test_post_data_form_empty_name():
+    # create pet
+    pet = new_pet()
+    post_pet_response = post_pet(pet)
+    assert post_pet_response.status_code == 200
+
+    post_pet_data = post_pet_response.json()
+
+    # check the created pet data
+    pet_id = post_pet_data["id"]
+    print(pet_id)
+    get_pet_response = get_pet(pet_id)
+    assert get_pet_response.status_code == 200
+    
+    get_pet_data = get_pet_response.json()
+    print(get_pet_data["name"], get_pet_data["status"]) # doggie, available
+    assert get_pet_data["name"] == pet["name"]
+    assert get_pet_data["status"] == pet["status"]
+        
+    # attempt to update with empty name
+    form = {
+        "name": "",
+        #"status": "sold"
+    }
+    
+    post_data_form_response = post_data_form(pet_id, form)
+    #need to check how name has been updated as not returning a 404 but 200
+    assert post_data_form_response.status_code == 400, f"Failed, gives {post_data_form_response.status_code} instead of 400"
+    
+
 ## API Calls
 def post_pet(pet):
     return requests.post(api + "/pet", json=pet)
