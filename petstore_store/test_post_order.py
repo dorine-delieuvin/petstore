14c1a41802643d7a251547539e0c5e18817de3df
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-08-25 11:53:36 +0100

    cleaned code

--- petstore_store/test_post_order.py
+++ petstore_store/test_post_order.py
@@ -1,6 +1,5 @@
 import pytest
 import requests
-import random
 import create_pet
 import create_order
 
@@ -12,11 +11,9 @@ def test_post_order():
     # create pet
     pet = create_pet.new_pet(pet_name="Pennies", status="available")
     pet_id = pet["id"]
-    print(pet_id)
 
     # create order
     order = create_order.new_order(pet_id)
-    print(order)
     post_order_response = post_order(order)
     assert (
         post_order_response.status_code == 200
@@ -29,17 +26,14 @@ def test_post_order():
 # python -m pytest -v -s .\test_post_order.py::test_post_order_unavailable_pet
 def test_post_order_unavailable_pet():
     '''
-    NOTE: orders can be placed for "sold" pets.
+    NOTE: orders can be placed for "pending" pets.
     '''
     # create pet
-    unavailable_pet = create_pet.new_pet(pet_name="Staying", status="sold")
+    unavailable_pet = create_pet.new_pet(pet_name="Staying", status="pending")
     pet_id = unavailable_pet["id"]
-    print(pet_id)
     
     # create order
     order = create_order.new_order(pet_id)
-    print(order)
-
     post_order_response = post_order(order)
     assert (
         post_order_response.status_code == 400
