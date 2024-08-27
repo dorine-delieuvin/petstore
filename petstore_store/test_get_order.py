81653d6f41280977c1083fdcc793319cc17a3af1
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-08-27 14:50:52 +0100

    test_get_order_unused_id

--- petstore_store/test_get_order.py
+++ petstore_store/test_get_order.py
@@ -29,6 +29,18 @@ def test_get_order():
     assert order[1]["petId"] == pet_id, f"{order["petId"]} instead of {pet_id}"
     assert order[1]["status"] == "placed", f"status: {order["status"]} instead of 'palced'"
 
+# python -m pytest -v -s .\test_get_order.py::test_get_order_unused_id
+def test_get_order_unused_id():
+    # do not create pet
+    # do not create order
+    # attempt getting order
+    unused_order_id = 0000
+    get_unused_order_id_response = get_order(unused_order_id)
+    assert get_unused_order_id_response.status_code == 404, f"Code {get_unused_order_id_response.status_code} instead of 404."
+    
+    get_invalid_order_data = get_unused_order_id_response.json()
+    assert get_invalid_order_data["message"] == "Order not found"
+    
 
 ## API calls
 def get_order(order_id):
