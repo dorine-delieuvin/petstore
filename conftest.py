79a0de269e02d17a71592dabb6c6ddf74f3c75e0
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-09-02 14:37:15 +0100

    test_get_login

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import random
+
+
+@pytest.fixture
+def user():
+    bob = {
+        "id": random.randint(1, 99999999999),
+        "username": "Bob" + str(random.randint(0, 10000)),
+        "firstName": "Troll",
+        "lastName": "Noob",
+        "email": "troll.noob@email.com",
+        "password": "pw123",
+        "phone": "07" + str(random.randint(000000000, 999999999)),
+    }
+    return bob
---
+++ petstore_user/test_get_login.py
@@ -0,0 +1,55 @@
+import pytest
+import requests
+
+pytestmark = pytest.mark.user
+
+
+def test_get_login(user):
+    # create user
+    post_user_response = post_user(user)
+    assert post_user_response.status_code == 200
+
+    # log in
+    details = [user["username"], user["password"]]
+
+    get_login_response = get_login(details[0], details[1])
+    assert get_login_response.status_code == 200
+
+    # check response body
+    get_login_data = get_login_response.json()
+    assert "logged in user session:" in get_login_data["message"]
+
+
+@pytest.mark.skip
+def test_get_login_incorrect_details(user):
+    """
+    NOTE: invalid details passed give a code 200 instead of 400.
+    """
+    # create user
+    post_user_response = post_user(user)
+    assert post_user_response.status_code == 200
+
+    # attempt log in
+    usernames = [user["username"], "", user["username"]]
+    passwords = ["invalidpw", user["password"], ""]
+
+    for i in passwords:
+        get_login_response = get_login(
+            usernames[passwords.index(i)], passwords[passwords.index(i)]
+        )
+        assert get_login_response.status_code == 400
+
+        # check response body
+        get_login_data = get_login_response.json()
+        assert get_login_data["message"] == "Invalid username/password supplied"
+
+
+## API Calls
+def post_user(user):
+    return requests.post("https://petstore.swagger.io/v2/user", json=user)
+
+
+def get_login(user_name, password):
+    return requests.get(
+        f"https://petstore.swagger.io/v2/user/login?username={user_name}&password=%20{password}"
+    )
