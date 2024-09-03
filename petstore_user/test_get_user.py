f501613645a826d7f5dd05b8aa7c500966754273
Dorine Delieuvin <dorine.delieuvin@gmail.com>
2024-09-03 14:17:08 +0100

    test_get_user

---
+++ petstore_user/test_get_user.py
@@ -0,0 +1,60 @@
+import pytest
+import requests
+
+pytestmark = pytest.mark.user
+
+
+def test_get_user(user):
+    # create user
+    post_user_response = post_user(user)
+    assert post_user_response.status_code == 200
+
+    # get user
+    get_user_response = get_user(user["username"])
+    assert get_user_response.status_code == 200
+
+    # check user details
+    get_user_data = get_user_response.json()
+    assert get_user_data["id"] == user["id"]
+    assert get_user_data["username"] == user["username"]
+    assert get_user_data["email"] == user["email"]
+    assert get_user_data["phone"] == user["phone"]
+
+
+def test_get_user_inexisting_username():
+    # do not create user
+    # attempt to get inexisting user
+    user_name = "Bob456"
+    get_user_response = get_user(user_name)
+    assert get_user_response.status_code == 404
+
+    # check body response
+    get_user_data = get_user_response.json()
+    assert get_user_data["message"] == "User not found"
+
+
+@pytest.mark.skip
+def test_get_user_invalid_username():
+    """
+    NOTE: getting code 404 instead of 400.
+    - tried usernames "Bob@!#", "@!#  !*/à$==`", "" (returns 405) and ",".
+    """
+    # do not create user
+    # attempt to get user with invalid username
+    user_name = "Bob@!#"
+    get_user_response = get_user(user_name)
+    assert get_user_response.status_code == 400
+
+    # check body response
+    get_user_data = get_user_response.json()
+    print(get_user_data)
+    assert get_user_data["message"] == "Invalid username supplied"
+
+
+## API Calls
+def post_user(user):
+    return requests.post("https://petstore.swagger.io/v2/user", json=user)
+
+
+def get_user(user_name):
+    return requests.get(f"https://petstore.swagger.io/v2/user/{user_name}")
